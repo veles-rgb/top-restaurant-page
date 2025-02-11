fffdb79459592e86888f882310355a3696c0a78c
MacMini <181009546+veles-rgb@users.noreply.github.com>
2025-02-11 14:04:03 -0500

    Create dynamic rendering of home.js
    Created renderHome function and exported to index.js

--- src/js/pages/home.js
+++ src/js/pages/home.js
@@ -1,1 +1,27 @@
 // home.js
+
+export default renderHome()
+
+function renderHome() {
+    const content = document.querySelector("#content");
+
+    // Append home container
+    const homeContainer = document.createElement("div");
+    homeContainer.className = "home-container";
+    content.appendChild(homeContainer);
+
+    // Append H1
+    const homeH1 = document.createElement("h1");
+    homeH1.textContent = "The Best Pub Around";
+    homeContainer.appendChild(homeH1);
+
+    // Append H2
+
+    const homeH2 = document.createElement("h2");
+    homeH2.textContent = "Fresh food, cold drinks and good times.";
+    homeContainer.appendChild(homeH2);
+
+    // Append P
+    const homeP = document.createElement("p");
+    homeP.textContent = "est. 1998";
+};
