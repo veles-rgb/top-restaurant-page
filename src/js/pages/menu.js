1403b32eac4cd64db90e4a46564d8a782fe899ba
MacMini <181009546+veles-rgb@users.noreply.github.com>
2025-02-11 16:36:23 -0500

    Setup menu.js and styles for menu

--- src/js/pages/menu.js
+++ src/js/pages/menu.js
@@ -1,6 +1,18 @@
 // menu.js
 
+// Create cards and make the menu container scrollable
 export default function renderMenu() {
     const content = document.querySelector("#content");
+
+    // Append menu container
+    const menuContainer = document.createElement("div");
+    menuContainer.id = "menu-container";
+    content.appendChild(menuContainer);
+
+    // Append h1 / title
+    const menuTitle = document.createElement("h1");
+    menuTitle.textContent = "MENU";
+    menuContainer.appendChild(menuTitle);
+
     
 };
