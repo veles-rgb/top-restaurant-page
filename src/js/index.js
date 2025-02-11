5f9c8334ff4363f15ad9c4a6d1b9232f1715a86e
MacMini <181009546+veles-rgb@users.noreply.github.com>
2025-02-11 14:05:51 -0500

    Import home.js and add button tab event listeners

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,22 @@
 // index.js
 import "../styles/reset.css";
 import "../styles/styles.css";
+import renderHome from "./pages/home";
+
+// Tab button click listeners
+const content = document.querySelector("#content")
+const tabBtns = document.querySelectorAll("nav button");
+tabBtns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        // Render different content for each button
+        if (e.target.textContent === "Home") {
+            content.replaceChildren();
+            renderHome();
+        } else if (e.target.textContent === "Menu") {
+            content.replaceChildren();
+        }
+    });
+});
+
+// Initial home page load
+renderHome();
