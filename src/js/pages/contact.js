2bfbc7b91d042da2282733b02caebe43df36f230
MacMini <181009546+veles-rgb@users.noreply.github.com>
2025-02-13 16:03:25 -0500

    Create contact page and styles

--- src/js/pages/contact.js
+++ src/js/pages/contact.js
@@ -3,4 +3,8 @@
 export default function renderContact() {
     const content = document.querySelector("#content");
 
+    // Create form container
+    const formContainer = document.createElement("div");
+    formContainer.id = "formContainer";
+    content.appendChild(formContainer);
 };
